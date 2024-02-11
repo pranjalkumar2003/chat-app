86dbdc4d78f6c1693c594cf0f444bedd9b9a3221
Pranjal Kumar Singh <pranjalkumarsingh4@gmail.com>
2024-02-11 14:11:01 +0530

    Create Signup End Point

--- api/index.js
+++ api/index.js
@@ -1,5 +1,6 @@
 import express  from "express";
 import dotenv from "dotenv";
+
 import authRoutes from './routes/auth.route.js';
 import connectToMongoDB from "./db/connectToMongoDB.js";
 
@@ -7,11 +8,14 @@ const app = express();
 
 dotenv.config();
 
+app.use(express.json());
+
+app.use("/api/auth",authRoutes);
+
 app.get("/",(req, res) => {
     res.send({message:"server is fetching data"});
 })
 
-app.use("/api/auth",authRoutes);
 
 app.listen(5000,()=>{
     connectToMongoDB();
