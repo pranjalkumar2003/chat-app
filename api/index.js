94c0efd00501c91b9f1f792ccc261f599612ce76
Pranjal Kumar Singh <pranjalkumarsingh4@gmail.com>
2024-02-11 11:49:23 +0530

    Connect to mongoDB

--- api/index.js
+++ api/index.js
@@ -1,9 +1,11 @@
 import express  from "express";
-
+import dotenv from "dotenv";
 import authRoutes from './routes/auth.route.js';
+import connectToMongoDB from "./db/connectToMongoDB.js";
 
 const app = express();
 
+dotenv.config();
 
 app.get("/",(req, res) => {
     res.send({message:"server is fetching data"});
@@ -12,5 +14,6 @@ app.get("/",(req, res) => {
 app.use("/api/auth",authRoutes);
 
 app.listen(5000,()=>{
-    console.log(new Date(Date.now()).toLocaleString().split(',')[1] + " || server is running on port 5000");
-})
+    connectToMongoDB();
+    console.log(new Date(Date.now()).toLocaleString().split(',')[1] + " || Server is running on port 5000 !!");
+});
