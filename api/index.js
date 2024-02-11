9f2641f5f16251055f4e7f501fbc3fe68af155cb
Pranjal Kumar Singh <pranjalkumarsingh4@gmail.com>
2024-02-11 19:23:47 +0530

    Create Protect Route Middleware

--- api/index.js
+++ api/index.js
@@ -1,6 +1,6 @@
 import express  from "express";
 import dotenv from "dotenv";
-
+import cookieParser from "cookie-parser";
 import authRoutes from './routes/auth.routes.js';
 import messageRoutes from './routes/message.routes.js';
 import connectToMongoDB from "./db/connectToMongoDB.js";
@@ -10,6 +10,7 @@ const app = express();
 dotenv.config();
 
 app.use(express.json());
+app.use(cookieParser());
 
 app.use("/api/auth",authRoutes);
 app.use("/api/messages",messageRoutes);
