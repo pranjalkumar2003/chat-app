dfb44ac9f57ad14fe08db3f2009fe17b082f813d
Pranjal Kumar Singh <pranjalkumarsingh4@gmail.com>
2024-02-11 10:55:04 +0530

    Done with project setup

--- api/index.js
+++ api/index.js
@@ -1,7 +1,12 @@
-const express = require("express");
+import express  from "express";
 
 const app = express();
 
+
+app.get("/",(req, res) => {
+    res.send({message:"server is fetching data"});
+})
+
 app.listen(5000,()=>{
-    console.log({message:"server is running on port 5000"});
+    console.log(new Date(Date.now()).toLocaleString().split(',')[1] + " || server is running on port 5000");
 })
