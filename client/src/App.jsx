01347d8c20e71b24d1d9d1a360c2d2bbf1216259
Pranjal Kumar Singh <pranjalkumarsingh4@gmail.com>
2024-02-16 03:45:34 +0530

    Done with UI design

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,13 +1,15 @@
-import Login from "./pages/login/Login"
+import Login from "./pages/login/Login";
+import SignUp from "./pages/signup/SignUp";
+import Home from "./pages/home/Home";
 
 
 function App() {
   return (
     <div className='p-4 h-screen flex items-center justify-center'>
-     <Login/>
+    <Home/>
      
     </div>
   )
 }
 
-export default App
+export default App;
