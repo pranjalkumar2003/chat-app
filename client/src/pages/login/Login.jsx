68c518677a0c08cf001dbe45295565001750c223
Pranjal Kumar Singh <pranjalkumarsingh4@gmail.com>
2024-02-16 04:40:38 +0530

    Partially Complete signup Functionality

--- client/src/pages/login/Login.jsx
+++ client/src/pages/login/Login.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import { Link } from 'react-router-dom';
 
 const Login = () => {
   return (
@@ -13,32 +14,32 @@ const Login = () => {
 
       <form>
         <div>
-						<label className='label p-2'>
-							<span className='text-base label-text'>Username</span>
-						</label>
-						<input
-							type='text'
-							placeholder='Enter username'
-							className='w-full input input-bordered h-10'
-							
-						/>
-					</div>
+			<label className='label p-2'>
+				<span className='text-base label-text'>Username</span>
+			</label>
+			<input
+				type='text'
+				placeholder='Enter username'
+				className='w-full input input-bordered h-10'
+				
+				/>
+		</div>
 
-          <div>
-						<label className='label'>
-							<span className='text-base label-text'>Password</span>
-						</label>
-						<input
-							type='password'
-							placeholder='Enter Password'
-							className='w-full input input-bordered h-10'
-							
-						/>
-					</div>
+        <div>
+			<label className='label'>
+				<span className='text-base label-text'>Password</span>
+			</label>
+			<input
+				type='password'
+				placeholder='Enter Password'
+				className='w-full input input-bordered h-10'
+				
+			/>
+		</div>
 
-          <a href='#' className='text-sm  hover:underline hover:text-blue-600 mt-2 inline-block'>
+		<Link to='/signup' className='text-sm  hover:underline hover:text-blue-600 mt-2 inline-block'>
 						{"Don't"} have an account?
-					</a>
+		</Link>
 
           <div>
  						<button className='btn btn-block btn-sm mt-2'>Login</button>
