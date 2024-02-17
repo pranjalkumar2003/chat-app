5dfc6d96b68b4c949f1b631dcd9689152e2ff96e
Pranjal Kumar Singh <pranjalkumarsingh4@gmail.com>
2024-02-17 21:42:55 +0530

    Create AuthContext

--- client/src/hooks/useSignup.js
+++ client/src/hooks/useSignup.js
@@ -1,9 +1,10 @@
 import { useState } from "react";
 import toast from "react-hot-toast";
+import { useAuthContext } from "../context/AuthContext";
 
 const useSignup = () => {
 	const [loading, setLoading] = useState(false);
-	
+	const { setAuthUser}= useAuthContext();
 
 	const signup = async ({ fullName, username, password, confirmPassword, gender }) => {
 		const success = handleInputErrors({ fullName, username, password, confirmPassword, gender });
@@ -18,7 +19,15 @@ const useSignup = () => {
 			});
 
 			const data = await res.json();
-            console.log(data);
+			if (data.error) {
+				throw new Error(data.error);
+			}
+           //console.log(data);
+
+		   //Local Storage
+		   localStorage.setItem("chat-user", JSON.stringify(data));
+		   //context
+			setAuthUser(data);
             
 		} catch (error) {
 			toast.error(error.message);
