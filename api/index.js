29f7230222e2776d50409e58881f695ffcc87584
Pranjal Kumar Singh <pranjalkumarsingh4@gmail.com>
2024-02-18 19:37:03 +0530

    Commit for deploy

--- api/index.js
+++ api/index.js
@@ -1,3 +1,4 @@
+import path from "path";
 import express  from "express";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
@@ -9,6 +10,7 @@ import userRoutes from './routes/user.routes.js';
 import connectToMongoDB from "./db/connectToMongoDB.js";
 import { app, server } from "./socket/socket.js";
 
+const __dirname = path.resolve();
 
 dotenv.config();
 
@@ -18,6 +20,12 @@ app.use(cookieParser());
 app.use("/api/auth",authRoutes);
 app.use("/api/messages",messageRoutes);
 app.use("/api/users",userRoutes);
+
+app.use(express.static(path.join(__dirname, "/client/dist")));
+
+app.get("*", (req, res) => {
+	res.sendFile(path.join(__dirname, "client", "dist", "index.html"));
+});
 
 app.get("/",(req, res) => {
     res.send({message:"server is fetching data"});
